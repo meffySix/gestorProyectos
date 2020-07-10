9c79552ae35a2cc5d8ce515187f665d086a6a100
afd <afd@alup5.academiapostal.es.local>
2020-07-10 18:37:02 +0200

    instalar ejs y plantilla index.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+//separar el proyecto en módulos para cada parte importante
+
 require("dotenv").config();
 const express = require ("express");
 const {Usuario, Rol, Proyecto, Tarea, Intervencion} = require("./models");
 const app = express();
 
+//motor de plantillas: app.set determina el funcionamiento de express
+//en qué carpeta están las plantillas de las vistas:
+app.set("views", "./views");
+//qué motor de plantillas se va a utilizar:
+app.set("view engine", "ejs");
 
+//definición de las rutas:
+app.get("/login", (req, res) => res.render("login"));
+app.post("/login", login);
 
 app.listen(3000);
