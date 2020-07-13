a07d4b92f1643bd21a2834dc6b442ee186354949
afd <afd@alup5.academiapostal.es.local>
2020-07-13 17:38:02 +0200

    Añadir md5 para ocultar las contraseñas

--- controllers/autenticacion.js
+++ controllers/autenticacion.js
@@ -1,8 +1,12 @@
-const {Usuario} = require ("../models");
+const {Usuario} = require("../models");
+const md5 = require("md5");
 
 function login(req, res) {
     const {email, password} = req.body;
-    Usuario.findOne({where: {email, password}})
+
+    // Buscamos un usuario en la base de datos que coincida con 
+    // el email y que la huella md5 de la contraseña coincida también.
+    Usuario.findOne({where: {email, password: md5(password)}})
     .then(usuario => {
         if (usuario) {
             req.session.usuario = usuario;
