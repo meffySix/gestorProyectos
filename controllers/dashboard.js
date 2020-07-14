6bc017fb8091c0604628c845245eafda7fbd32fc
afd <afd@alup5.academiapostal.es.local>
2020-07-14 19:34:58 +0200

    Abreviación de fecha (moment)

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,4 +1,5 @@
 const {Usuario, Tarea} = require("../models");
+const moment = require("moment");
 
 function dashboard(req, res) {
     // .session -> información de sesión de las cookies (cookie-session):
@@ -7,7 +8,16 @@ function dashboard(req, res) {
         include: {model: Tarea, as: "tareas"}
     })
     .then(usuario => {
-        const tarea = usuario.tareas;
+        // pillamos las tareas y aplicamos lo siguiente a todas ellas (map):
+        const tarea = usuario.tareas.map(tarea => {
+            return {
+                // recogemos los parámetros de la base de datos (nombre, fecha_inicio, fecha_vencimiento) y las definimos
+                // una por una, siendo las fechas pasadas por la librería "moment" para mostrar solo (DD/MM/YYYY)
+                nombre: tarea.nombre,  
+                fecha_inicio: moment(tarea.fecha_inicio).format("DD/MM/YYYY"), 
+                fecha_vencimiento: tarea.fecha_vencimiento && moment(tarea.fecha_vencimiento).format("DD/MM/YYYY")
+            }
+        });
         res.render("dashboard", {usuario, tarea})
     }) 
 }
