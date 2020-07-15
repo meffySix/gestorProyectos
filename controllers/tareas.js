8f035003bfb8d02f387c3d2c26e6990693f660e0
afd <afd@alup5.academiapostal.es.local>
2020-07-15 18:16:21 +0200

    Fallos Escritura

--- controllers/tareas.js
+++ controllers/tareas.js
@@ -1,16 +1,19 @@
-const { Tarea, Intervencion, Usuario } = require("../models/tareas");
+const { Tarea, Intervencion, Usuario } = require("../models");
 const moment = require("moment");
 
 function mostrarTarea(req, res) {
     const id = req.params.id;
-    Tarea.findByPk(id, {include: [Intervencion]})
+    // intervencione viene de la relación establecida en el index.js (as: "intervencione"):
+    Tarea.findByPk(id, {include: ["intervencione"]})
     .then(tarea => {
         res.render("tarea", {
             tarea: {
+                id: tarea.id, 
+                nombre: tarea.nombre, 
                 link: "/tareas/" + tarea.id, 
                 estado: tarea.fecha_fin? "Finalizado" : "Pendiente", 
-                intervenciones: tarea.intervencions, 
-                iniciada: tarea.intervencions.some(x => x.fin==null), 
+                intervencione: tarea.intervencione, 
+                iniciada: tarea.intervencione.some(x => x.fin==null), 
                 fecha_inicio: moment(tarea.fecha_inicio).format("DD/MM/YYYY"), 
                 fecha_fin: tarea.fecha_fin && moment(tarea.fecha_fin).format("DD/MM/YYYY"), 
                 fecha_vencimiento: tarea.fecha_vencimiento && moment(tarea.fecha_vencimiento).format("DD/MM/YYYY")
@@ -18,6 +21,8 @@ function mostrarTarea(req, res) {
         })
     })
 }
+
+
 
 module.exports = {
     mostrarTarea
