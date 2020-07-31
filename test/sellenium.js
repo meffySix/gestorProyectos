0b494ab33ed4dcd525effdd3b50b55e98c66b229
afd <afd@alup5.academiapostal.es.local>
2020-07-31 18:56:20 +0200

    Test conexión fallida

--- test/sellenium.js
+++ test/sellenium.js
@@ -18,7 +18,26 @@ describe("Formulario de login", function() {
             assert.equal("Bienvenido, Daniel", await firstResult.getAttribute("textContent"));
         }
         finally{
-            driver.quit();
+            await driver.quit();
+        }
+    })
+
+    it("Iniciar sesión con éxito", async function() {
+        this.timeout(10000);
+
+        let driver = await new Builder().forBrowser('chrome').build();
+        try {
+            // Navigate to our site.
+            await driver.get('http://localhost:3000/login');
+            // Enter the form and perform keyboard action "Enter"
+            await driver.findElement(By.name('email')).sendKeys('daniel@gmail.com');
+            await driver.findElement(By.name('password')).sendKeys('2193879213', Key.ENTER);
+
+            let firstResult = await driver.wait(until.elementsLocated(By.className('alert alert-danger')), 10000);
+            assert.ok(firstResult);
+        }
+        finally{
+            await driver.quit();
         }
     })
 })
